8f53b42e1787ebea1dcf1c075f7f19154ea35ce0
ivanj09 <33155685+ivanj09@users.noreply.github.com>
2018-03-10 22:22:28 +0700

    Update Snail.hpp

--- Snail.hpp
+++ Snail.hpp
@@ -7,15 +7,21 @@
 class Snail{
     private:
         Location location;
+        int currentCoin; //Banyaknya koin sekarang yang ditampung Snail
         Coin* coin;     //Buat nyimpen koin(?) (MARK)
     public:
         Snail();
-        Coin getCoin()const; // (MARK)
+        ~Snail();
+        Coin* getCoins()const; // (MARK)
         void insertCoin(Coin&); //Aggregation (MARK)
         /* KASUS I  = Kalau ada koin dia didekatnya, dia gerak ke lokasi koin
          * KASUS II = Kalau ga ada koin, dia diem
         */
         void Move(); 
+    
+        /*Get & Set currentCoin*/
+        void setCurrentCoin(int);
+        int getCurrentCoin()const;
 };
 
 #endif

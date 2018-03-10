90b650e11c0263394fe4dc61591ceb14970404a4
Ivan Jonathan <ivanj_@Mi>
2018-03-10 22:51:54 +0700

    Note: Buat yang ada kata MARK, tolong di-check ya

---
+++ LinkedList.hpp
@@ -0,0 +1,29 @@
+#ifndef LL_HPP
+#define LL_HPP
+
+template <class T>
+class LinkedList{
+    private:
+        int currentSize;
+        bool isEmpty;
+        T* Head;
+        T* Tail;
+    public:
+        LinkedList();
+        ~LinkedList();
+        
+        /*Get & Set IsEmpty*/
+        bool getIsEmpty() const;
+        void setIsEmpty(bool);
+
+        /*Get & Set currenSize*/
+        void setCurrentSize(int);
+        int getCurrentSize(int);
+
+        void find(T);
+        void add(T);
+        void remove(T*);
+        int getIndex() const;
+};
+
+#endif

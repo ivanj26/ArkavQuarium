916023ad5616cad18c5ddb2e9b4c1599632ae2e0
RegiPurba <regipurba07@gmail.com>
2018-04-09 09:26:02 +0700

    Add files via upload

--- src/Aquarium/Aquarium.cpp
+++ src/Aquarium/Aquarium.cpp
@@ -1,18 +1,25 @@
 #include "../../include/Aquarium.hpp"
+#include "../../include/Animal/Guppy.hpp"
+#include "../../include/Animal/Piranha.hpp"
+#include "../../include/Animal/Snail.hpp"
+#include "../../include/LinkedList/Node.hpp"
+#include "../../include/LinkedList/LinkedList.hpp"
 #include <iostream>
 using namespace std;
 
 Aquarium::Aquarium(){
 	//Menginisiasi Guppy dan insert ke LinkedList of Guppy
-	Guppy guppy;
-	guppy.setX(generateRandom(40,SCREEN_WIDTH-40));
-	guppy.setY(generateRandom(115,SCREEN_HEIGHT-40));
-	guppy.setGrowLevel(1);
-	guppy.setCoinTime(0);
-	guppy.setNbFood(0);
-	Guppies.add(guppy);
+	Guppy *guppy= new Guppy();
+	guppy->setX(generateRandom(40,SCREEN_WIDTH-40));
+	guppy->setY(generateRandom(115,SCREEN_HEIGHT-40));
+	guppy->setGrowLevel(1);
+	guppy->setCoinTime(0);
+	guppy->setNbFood(0);
+	Fishes.add(guppy);
+	guppy = nullptr;
+	delete guppy;
 	//Menginisiasi Piranha
-	Piranha piranha;
+	Piranha *piranha = new Piranha();
 
 }
 
@@ -20,14 +27,14 @@ Snail Aquarium::getSnail(){
 	return this->snail;
 }
 
-LinkedList<Guppy> Aquarium::getGuppies(){
-	return this->Guppies;
+LinkedList<Fish*> Aquarium::getFishes(){
+	return this->Fishes;
 }
 
-LinkedList<Coin> Aquarium::getCoins(){
+LinkedList<Coin*> Aquarium::getCoins(){
 	return this->Coins;
 }
 
-LinkedList<Food> Aquarium::getFoods(){
+LinkedList<Food*> Aquarium::getFoods(){
 	return this->Foods;
 }

f3f2c85f07b21932ffe36d2370d7ddf68bf8b7e9
RegiPurba <regipurba07@gmail.com>
2018-04-09 07:58:54 +0700

    Add files via upload

--- src/Aquarium/Aquarium.cpp
+++ src/Aquarium/Aquarium.cpp
@@ -3,30 +3,31 @@
 using namespace std;
 
 Aquarium::Aquarium(){
-	Fishes = new LinkedList<Fish>();
-	Coins = new LinkedList<Coin>();
-	Foods = new LinkedList<Food>();
-	Guppy guppy();
+	//Menginisiasi Guppy dan insert ke LinkedList of Guppy
+	Guppy guppy;
 	guppy.setX(generateRandom(40,SCREEN_WIDTH-40));
-	guppy.setY(generateRandom(115,SCREEN_HEIGHT-40))
+	guppy.setY(generateRandom(115,SCREEN_HEIGHT-40));
 	guppy.setGrowLevel(1);
 	guppy.setCoinTime(0);
 	guppy.setNbFood(0);
-	Fishes.add(guppy);
+	Guppies.add(guppy);
+	//Menginisiasi Piranha
+	Piranha piranha;
+
 }
 
 Snail Aquarium::getSnail(){
 	return this->snail;
 }
 
-LinkedList<Fish> Aquairum::getFishes(){
-	return this->Fishes;
+LinkedList<Guppy> Aquarium::getGuppies(){
+	return this->Guppies;
 }
 
-LinkedList<Fish> Aquarium::getCoins(){
+LinkedList<Coin> Aquarium::getCoins(){
 	return this->Coins;
 }
 
-LinkedList<Fish> Aquarium::getFoods(){
+LinkedList<Food> Aquarium::getFoods(){
 	return this->Foods;
 }

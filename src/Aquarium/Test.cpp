c27a64e7faffc8525b1f6860834b989e4300bb96
ivanj09 <xmilleniumij@gmail.com>
2018-04-09 08:55:37 +0700

    Update

--- src/Aquarium/Test.cpp
+++ src/Aquarium/Test.cpp
@@ -6,43 +6,35 @@
 using namespace std;
 
 int main(){
-  LinkedList<Fish*> linkedList;
-  Guppy *guppy = new Guppy();
+  LinkedList<Fish*> linkedList; //Buat linkedlist of fish pointer soalnya ga bisa langsung fish (krn abstract)
+  Guppy *guppy = new Guppy(); //Buat guppy pointer soalnya dengan method "add", sama aja kayak
+                                                  //Fish* f = new Guppy();
   guppy->setX(9);
   guppy->setY(10);
-
-  linkedList.add(guppy);
-  guppy = nullptr;
-
-  Guppy *i = new Guppy();
-  i->setX(89);
-  i->setY(100);
-
-  linkedList.add(i);
-  i = nullptr;
-
-  i = new Guppy();
-  i->setX(6);
-  i->setY(100);
-
-  linkedList.add(i);
+  linkedList.add(guppy); //add elmt 1
+  guppy = nullptr;      //wajib jadiin nullptr
   delete guppy;
-  delete i;
 
-  // for (int i = 0; i < linkedList.getCurrentSize(); i++){
-  //   Guppy *t = dynamic_cast<Guppy*>(linkedList.getIndex(i));
-  //   cout << t->getX() << " " << t->getY() << endl;
-  //   t = nullptr;
-  // }
-  Fish *u = (linkedList.getIndex(2));
-  cout << " " << u->getX() << endl;
-  Guppy *t = dynamic_cast<Guppy*>(linkedList.getIndex(1));
-  cout << " " << t->getX() << endl;
-  // Guppy guppy;
-  //
-  // guppy.setX(25);
-  // guppy.setY(70);
-  //
-  // fishes.add(guppy);
+  guppy = new Guppy();
+  guppy->setX(89);
+  guppy->setY(100);
+
+  linkedList.add(guppy); //add elmt2
+  guppy = nullptr;
+  delete guppy;
+
+  guppy = new Guppy();
+  guppy->setX(6);
+  guppy->setY(100);
+
+  linkedList.add(guppy); //add elmt 3
+  guppy = nullptr;
+delete guppy;
+  Fish* f = linkedList.getIndex(0);
+  if (f->getID() == 0){
+    cout << "This is guppy!" << endl;
+    Guppy *t = dynamic_cast<Guppy*>(linkedList.getIndex(2));
+    cout << " " << t->getX() << endl;
+  }
   return 0;
 }
